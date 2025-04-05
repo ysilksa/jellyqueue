a566915883ef88d322aa5e22fcaa133acdce70cb
vichedanarith <vichedanarith2027@u.northwestern.edu>
2025-04-05 16:31:50 -0500

    authentification button

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
 
   return (
-    <>
-      <h1>GOOGLE CALENDAR</h1>
-    </>
-  )
+    <GoogleOAuthProvider clientId=" 274207504667-3mt33k26gif5aujrlrgvvtltr1go51ps.apps.googleusercontent.com">
+      <div className="App">
+        <p>Jelly Queue</p>
+        <GoogleLogin
+          onSuccess={responseGoogle}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </div>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
