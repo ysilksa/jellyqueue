94ac37bfd536f15ec2dd045d23a7ab338c29ba9c
katiejg <katiejiang@proton.me>
2025-04-05 16:48:37 -0500

    Update .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { GoogleOAuthProvider } from '@react-oauth/google' // for Google Sign-In
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </StrictMode>,
 )
