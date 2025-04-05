27020cd932d42ebec7e43936de3cc7e7aa6401b2
katiejg <katiejiang@proton.me>
2025-04-05 15:51:53 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import SignIn from "./components/SignIn";
 // import Home from "./components/Home";
 import React from "react";
 
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
