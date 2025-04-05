78206821d92aa3b7a2059110561727ed90f9a063
katiejg <katiejiang@proton.me>
2025-04-05 16:48:53 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google' // for Google Sign-In
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
